6d42abd4ab29919cbce74a213c7d85c999c7bd0a
your name <your email>
2019-12-25 14:50:23 +0800

    update cookie

--- src/main/java/com/xcy/community2/controller/IndexController.java
+++ src/main/java/com/xcy/community2/controller/IndexController.java
@@ -15,19 +15,22 @@ public class IndexController {
     UserMapper userMapper;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie : cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies == null) {
+            return "index";
+        } else {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
+            return "index";
         }
-        return "index";
     }
-
 }
